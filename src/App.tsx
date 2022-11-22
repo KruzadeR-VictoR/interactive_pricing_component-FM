20c44ea2bde29ac0656acd25862c4323d853ce2d
KruzadeR-VictoR <victorbiju31@gmail.com>
2022-11-22 20:59:06 +0530

    fixed the range slider

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import bgPattern from "./assets/images/bg-pattern.svg";
 import circles from "./assets/images/pattern-circles.svg";
@@ -6,13 +6,14 @@ import slider from "./assets/images/icon-slider.svg";
 import check from "./assets/images/icon-check.svg";
 
 function App() {
-  const [isActive, setisActive] = useState(true)
+  const [isActive, setisActive] = useState(true);
+  const [Value, setValue] = useState<number>(50);
+  const rangeRef = useRef<HTMLInputElement | any>(null);
 
-const handleToggle=()=>{
-    setisActive(prev=>!prev)
-    console.log(isActive)
-  }
-
+  const handleToggle = () => {
+    setisActive((prev) => !prev);
+    console.log(isActive);
+  };  
 
   const style = {
     App: "min-h-screen bg-VeryPaleBlue",
@@ -28,25 +29,39 @@ const handleToggle=()=>{
     // top
     top_section: "flex flex-col gap-10 md:gap-[3rem] p-5 pt-[2.5rem] md:p-10",
     card_heading:
-      "text-center md:text-left text-[.8rem] font-800 uppercase text-GrayishBlue tracking-[2px]",
-    slider: "slider bg-LightGrayishBlue1",
-    price: "flex justify-center items-center gap-3 text-GrayishBlue md:absolute md:right-[2rem] md:top-[1.5rem]",
+      "text-center md:text-left text-[.8rem] font-800 uppercase text-GrayishBlue tracking-[2px]",    
+    price:
+      "flex justify-center items-center gap-3 text-GrayishBlue md:absolute md:right-[2rem] md:top-[1.5rem]",
     amount: "text-4xl font-800 text-DarkDesaturatedBlue",
-    toggle:'toggle_switch bg-LightGrayishBlue2',
-    toggleActive:'toggle_switch bg-LightGrayishBlue2 active',
+    toggle: "toggle_switch bg-LightGrayishBlue2",
+    toggleActive: "toggle_switch bg-LightGrayishBlue2 active",
     Billing_type: "flex justify-between items-center gap-3 mx-auto",
     billing_text: "text-[.82rem] text-GrayishBlue",
     off_pecentage:
       "text-[.7rem] ml-1 bg-LightGrayishRed text-LightRed rounded-full px-2 py-[.1rem]",
     // bottom
-    bottom_section: "flex flex-col md:flex-row items-center gap-8 p-5 pb-9 md:p-8",
+    bottom_section:
+      "flex flex-col md:flex-row items-center gap-8 p-5 pb-9 md:p-8",
     features: "flex flex-col items-center md:items-start gap-3",
     feat: "text-[.8rem] text-GrayishBlue flex items-center gap-4",
-    startBtn_wrap:'flex-1 md:flex md:justify-end',
+    startBtn_wrap: "flex-1 md:flex md:justify-end",
     startBtn:
       "bg-DarkDesaturatedBlue text-PaleBlue tracking-wider text-[.8rem] p-3 px-[3rem] rounded-full cursor-pointer hover:text-White",
   };
 
+  // sets the style to slider on load 
+  useEffect(() => {
+    console.log(rangeRef.current?.style.background);
+    let valPercent:any = (rangeRef.current?.value / rangeRef.current?.max) * 100;    
+    rangeRef.current.style.background=`linear-gradient(to right, hsl(174, 77%, 80%) ${valPercent}%, hsl(224, 65%, 95%) ${valPercent}%)`
+  }, []);
+
+  const handleValue = (e: any) => {
+    console.log(e);
+    setValue(e.target.value);
+    let valPercent = (e.target.value / e.target.max) * 100;
+    e.target.style.background = `linear-gradient(to right, hsl(174, 77%, 80%) ${valPercent}%, hsl(224, 65%, 95%) ${valPercent}%)`;
+  };
 
   return (
     <main className={style.App}>
@@ -63,25 +78,33 @@ const handleToggle=()=>{
       {/* Card  */}
       <div className={style.card}>
         <div className={style.top_section}>
-          <h2 className={style.card_heading}>100K Pageviews</h2>
-          <div className={style.slider}>
-            <div className="progress bg-SoftCyan"></div>
-            <div className="slider_icon bg-StrongCyan">
-              <img src={slider} alt="slider" className="icon" />
-            </div>
-          </div>
-          {/* <label htmlFor="Slider" aria-hidden='true'></label> */}
-          {/* <input type="range" min="0" max="100" value='50' id="Slider"/> */}
+          <h2 className={style.card_heading}>100K Pageviews</h2>         
+          <label htmlFor="Slider" aria-hidden="true"></label>
+          <input
+            type="range"
+            ref={rangeRef}
+            min="0"
+            max='100'
+            value={Value}
+            onChange={(e) => handleValue(e)}
+          />
           <div className={style.price}>
             <span className={style.amount}>$16.00</span>/ month
           </div>
           <div className={style.Billing_type}>
             <div className={style.billing_text}>Monthly Billing</div>
-            <div className={isActive ? style.toggle : style.toggleActive} onClick={handleToggle}>
+            <div
+              className={isActive ? style.toggle : style.toggleActive}
+              onClick={handleToggle}
+            >
               <div className="toggle_icon bg-White"></div>
             </div>
             <div className={style.billing_text}>
-              Yearly Billing <span className={style.off_pecentage}>-25% <span className="hidden md:inline md:ml-[.1rem]">discount</span></span>
+              Yearly Billing{" "}
+              <span className={style.off_pecentage}>
+                -25%{" "}
+                <span className="hidden md:inline md:ml-[.1rem]">discount</span>
+              </span>
             </div>
           </div>
         </div>
